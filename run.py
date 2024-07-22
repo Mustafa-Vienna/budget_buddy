6167bbc8e4ba2f0cb083f5211d43a2a20a3da6ec
Mustafa Muhammed <mustafa860505@hotmail.com>
2024-07-22 14:29:38 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,25 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import os
+import sys
+import random
+import time
+
+os.system('clear')
+
+print()
+print("********************************************")
+print("*                                          *")
+print("*          Welcome to Budget Buddy !       *")
+print("*                                          *")
+print("*    🏠 Calculate your income & expenses   *")
+print("*      on a monthly basis. Print results   *")
+print("*               to the screen.             *")
+print("*                                          *")
+print("*      📈 Please enter whole numbers       *")
+print("*       only. No decimals, Please !!       *")
+print("*                                          *")
+print("********************************************")
+print()
+print("Let's see if you are a Richie Rich 💷 or a Brokey Broke 😲 after this month! ")
+print("Prepare for a fun ride through your finances! 🚀🤑＄＄")
+print()
+time.sleep(0.5)
